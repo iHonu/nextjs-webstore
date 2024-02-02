193b8c48ac4240d1bd88ec306c91d4333ccb5381
Anastasia Tikhonova <honu347@gmail.com>
2024-02-02 22:23:14 +0100

    started stripe

--- components/ShoppingCartModal.tsx
+++ components/ShoppingCartModal.tsx
@@ -6,10 +6,19 @@ import {
   SheetHeader,
   SheetTitle,
 } from '@/components/ui/sheet';
+import Image from 'next/image';
 import { useShoppingCart } from 'use-shopping-cart';
+import { Button } from './ui/button';
 
 export default function ShoppingCartModal() {
-  const { cartCount, shouldDisplayCart, handleCartClick } = useShoppingCart();
+  const {
+    cartCount,
+    shouldDisplayCart,
+    handleCartClick,
+    cartDetails,
+    removeItem,
+    totalPrice,
+  } = useShoppingCart();
 
   return (
     <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
@@ -23,9 +32,71 @@ export default function ShoppingCartModal() {
               {cartCount === 0 ? (
                 <h1 className="py-6">You dont have any items</h1>
               ) : (
-                <h1>You have some items</h1>
+                <>
+                  {Object.values(cartDetails ?? {}).map((entry) => (
+                    <li key={entry.id} className="flex py-6 ">
+                      <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-mb border border-gray-200">
+                        <Image
+                          src={entry.image as string}
+                          alt="Product image"
+                          width={100}
+                          height={100}
+                        />
+                      </div>
+
+                      <div className="ml-4 flex flex-1 flex-col">
+                        <div>
+                          {' '}
+                          <div className="flex justify-between text-base font-medium text-gray-900">
+                            <h3>{entry.name}</h3>
+                            <p>{entry.price}€</p>
+                          </div>
+                        </div>
+
+                        <p className="mt-1 text-sm text-gray-500 line-clamp-2">
+                          {entry.description}
+                        </p>
+                        <div className="flex flex-1 items-end justify-between text-sm">
+                          <p className="text-gray-500">{entry.quantity}</p>
+                          <div className="flex">
+                            <button
+                              type="button"
+                              className="font-medium text-primary hover:text-primary/80"
+                              onClick={() => removeItem(entry.id)}
+                            >
+                              Remove
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </>
               )}
             </ul>
+          </div>
+          <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+            <div className="flex justify-between tet-base font-medium text-gray-900">
+              <p>Subtotal:</p>
+              <p>{totalPrice}€</p>
+            </div>
+            <p className="mt-0.5 text-sm text-gray-500">
+              Shipping and taxes are calculated at checkout
+            </p>
+            <div className="mt-6">
+              <Button className="w-full">Checkout</Button>
+            </div>
+            <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+              <p>
+                OR
+                <button
+                  onClick={() => handleCartClick()}
+                  className="font-medium text-primary hover:text-primary/80"
+                >
+                  continue shopping
+                </button>
+              </p>
+            </div>
           </div>
         </div>
       </SheetContent>
