40fc4364ed3a67d6b5fcf84fc300dc3defe23da6
Anastasia Tikhonova <honu347@gmail.com>
2024-02-08 13:45:00 +0100

    cashing fixed

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -12,8 +12,8 @@ export default function CartProvider({
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="http://nextjs-webstore-three/stripe/success"
+      cancelUrl="http://nextjs-webstore-three/stripe/error"
       currency="EUR"
       billingAddressCollection={false}
       shouldPersist={true}
