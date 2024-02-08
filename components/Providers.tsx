7c6c936b2459a27d60ba1c220da698fb6cd4c947
Anastasia Tikhonova <honu347@gmail.com>
2024-02-08 13:52:26 +0100

    typo

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -12,8 +12,8 @@ export default function CartProvider({
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://https://nextjs-webstore-three.vercel.app//stripe/success"
-      cancelUrl="http://https://nextjs-webstore-three.vercel.app//stripe/error"
+      successUrl="https://nextjs-webstore-three.vercel.app//stripe/success"
+      cancelUrl="https://nextjs-webstore-three.vercel.app//stripe/error"
       currency="EUR"
       billingAddressCollection={false}
       shouldPersist={true}
